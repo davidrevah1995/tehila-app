501086bd90464ea806a724618ca70c5ef6b46237
davidrevah1995 <davidrevah95@gmail.com>
2026-09-04 00:14:28 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tehila-ledavid-v1';
+const CACHE_NAME = 'tehila-ledavid-v18';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
